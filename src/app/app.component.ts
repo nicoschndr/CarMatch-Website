bfb62075544d48a7fecbb158975628376d849d05
nico- <nico.schneider-2@mni.thm.de>
2023-10-11 13:59:31 +0200

    implemented angular router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,12 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'CarMatch-Website';
-  isSmallNav = false;
-  isSticky = false;
-  isLight = true;
+export class AppComponent{
 
-  firstName = "";
-  lastName = "";
-  email = "";
-  message = "";
-
-  currentLanguage = "deutsch";
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event): void {
-    this.isSmallNav = window.innerWidth < 800;
-  }
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-    const scrollPosition = window.pageYOffset;
-
-    if (scrollPosition >= 10) {
-      this.isSticky = true;
-      this.isLight = false;
-    } else {
-      this.isSticky = false;
-      this.isLight = true;
-    }
-  }
-
-  sendMessage(){
-    console.log(this.firstName, this.lastName, this.email, this.message)
-  }
-
-  ngOnInit(): void {
-    this.isSmallNav = window.innerWidth < 800;
-    this.currentLanguage = localStorage.getItem('language')!
-  }
-
-  setCurrentLanguage(language: string){
-    this.currentLanguage = language;
-    localStorage.setItem('language', language)
-  }
-
-  setScrollOffset() {
-    if (this.isLight && !this.isSticky){
-      this.isLight = false
-    } else if(!this.isLight && !this.isSticky) {
-      this.isLight = true
-    }
-  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     NgOptimizedImage,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [{provide: Window, useValue: window}],
   bootstrap: [AppComponent]
